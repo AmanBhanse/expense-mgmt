cd7d400c1c474463939914769005fc9aed3fd90c
AmanBhanse <amanbhanse22@gmail.com>
2024-12-18 11:45:20 +0100

    Header + Sidebar

--- src/pages/sidebar/index.js
+++ src/pages/sidebar/index.js
@@ -1,3 +1,25 @@
+import React from "react";
+
+
 export const SideBar = () => {
-    return "sidebar";
+  return (
+    <div className="d-flex flex-column vh-100 bg-dark text-white p-3" style={{ width: "250px" }}>
+      {/* Sidebar Header */}
+      <h2 className="text-center mb-4">Hello Adriano!</h2>
+
+      {/* Sidebar Links */}
+      <ul className="nav nav-pills flex-column">
+        <li className="nav-item mb-2">
+          <a href="#expense" className="nav-link text-white">
+            Expense
+          </a>
+        </li>
+        <li className="nav-item mb-2">
+          <a href="#analysis" className="nav-link text-white">
+            Analysis
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
